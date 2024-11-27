8d1269c2f8cc867a74b479fac955cf9284d9c711
Asad9340 <asadulimran1999@gmail.com>
2024-11-27 23:18:42 +0600

    added new system for delete

--- app/data/messageData.tsx
+++ app/data/messageData.tsx
@@ -22,3 +22,9 @@ export const getAllMessages = async () => {
   const data = await fs.readFile(filePath, 'utf-8');
   return JSON.parse(data);
 }
+export const deleteMessage = async (id: string): Promise<void> => {
+  const filePath = path.join(process.cwd(), 'messages.json');
+  const messages: Message[] = JSON.parse(await fs.readFile(filePath, 'utf-8'));
+  const updatedMessages = messages.filter(message => message.id !== id);
+  await fs.writeFile(filePath, JSON.stringify(updatedMessages, null, 2));
+};
--- app/routes/services.tsx
+++ app/routes/services.tsx
@@ -1,17 +1,33 @@
-import { useLoaderData } from '@remix-run/react';
-import { getAllMessages } from '~/data/messageData';
+import { ActionFunctionArgs } from '@remix-run/node';
+import { Form, useLoaderData } from '@remix-run/react';
+import { deleteMessage, getAllMessages, Message } from '~/data/messageData';
+
+
 
 export async function loader() {
-  const data = await getAllMessages();
+  const data: Message[] = await getAllMessages();
   return data;
 }
-function services() {
-  const data = useLoaderData();
-  console.log(data);
+
+export async function action({ request }: ActionFunctionArgs) {
+  const method = await request.formData();
+
+  const id = method.get('delete');
+  if (typeof id !== 'string') {
+    throw new Error('Invalid form data');
+  }
+
+  await deleteMessage(id)
+  return {message:'success'}
+
+}
+function Services() {
+  const data = useLoaderData<Message[]>();
+
   return (
-    <div className='grid grid-cols-1 md:grid-cols-3 mt-6 max-w-5xl mx-auto gap-4'>
+    <div className="grid grid-cols-1 md:grid-cols-3 mt-6 max-w-5xl mx-auto gap-4">
       {data &&
-        data?.map(message => (
+        data.map(message => (
           <div
             key={message.id}
             className="w-full max-w-sm p-4 bg-white rounded-lg shadow-md border border-gray-200"
@@ -25,13 +41,21 @@ function services() {
             <p className="text-gray-600">
               <span className="font-medium">Message:</span> {message.message}
             </p>
-            <span className="block mt-2 text-sm text-gray-500">
-              {message.id}
-            </span>
+            <p className="flex justify-between gap-4">
+              <span className="block mt-2 text-sm text-gray-500">
+                {message.id}
+              </span>
+              <span>
+                <Form method="POST">
+                  <input type="hidden" name="delete" value={message?.id} />
+                  <button type='submit' className="border px-2 py-1 rounded-lg">Delete</button>
+                </Form>
+              </span>
+            </p>
           </div>
         ))}
     </div>
   );
 }
 
-export default services;
+export default Services;
